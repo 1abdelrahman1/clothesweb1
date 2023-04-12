e840c81f9372e37c8a644f2ac7251a40a7606389
1abdelrahman1 <97918409+1abdelrahman1@users.noreply.github.com>
2023-04-12 22:17:40 +0200

    Add files via upload

---
+++ clothes-pages/app.js
@@ -0,0 +1,10 @@
+const products = [
+    {
+      id: 0,
+      name: "T-shirt 1",
+      price: 29.99,
+      instock: 100,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/t1.png",
+    }]
